polux/defunc

commit e054fb335c22e7aeba95ecf25da5ff1f8756f345

use embed for let

src/CPS.hs
@@ -38,7 +38,7 @@ termCps t = do
   return (lam k t')
 
 serious (App _ _) = True
-serious (Let _ _) = True
+serious (Let _) = True
 serious (Match _ _) = True
 serious (Cons _ ts) = any serious ts
 serious _ = False
@@ -88,10 +88,10 @@ sCps (Match t rs) k
 sCps t@(Cons _ _) k = do
   (cs, t) <- comps t
   cCps cs (k @: t)
-sCps (Let b t) k = do
-  (p, u) <- unbind b
+sCps (Let b) k = do
+  ((p, t), u) <- unbind b
   u' <- eCps u k
-  eCps t (plam p u')
+  eCps (unembed t) (plam p u')
 sCps (App t0 t1) k
   | serious t0 && serious t1 = do
       x0 <- fresh (string2Name "x")

src/AST.hs
@@ -57,7 +57,7 @@ data Term
   = Var (Name Term)
   | App Term Term
   | Lam (Bind Pattern Term)
-  | Let (Bind Pattern Term) Term
+  | Let (Bind (Pattern, (Embed Term)) Term)
   | Lit Int
   | Cons String [Term]
   | Match Term Rules

src/Defunc.hs
@@ -79,11 +79,11 @@ saturate fs t = go t
       (p, t) <- unbind b
       t' <- go t
       return (plam p t')
-    go (Let b t) = do
-      (p, u) <- unbind b
+    go (Let b) = do
+      ((p, t), u) <- unbind b
       u' <- go u
-      t' <- go t
-      return (Let (bind p u') t)
+      t' <- go (unembed t)
+      return (Let (bind (p, embed t') u'))
     go l@(Lit _) = return l
     go (Cons f ts) = Cons f <$> mapM go ts
     go (Match t rs) = Match <$> go t <*> mapM goRule rs
@@ -167,14 +167,14 @@ defuncTerm fs apply arg term = go term
     let pclos = PCons c (map PVar fns)
     let rhs = case p of
                 (PVar x) -> subst x (Var arg) e'
-                _ -> Let (bind p e') (Var arg)
+                _ -> Let (bind (p, embed (Var arg)) e')
     tell (S.singleton (Rule $ bind pclos rhs))
     return $ Cons c (map Var fns)
-  go (Let b t) = do
-    (p, u) <- unbind b
-    t' <- go t
+  go (Let b) = do
+    ((p, t), u) <- unbind b
+    t' <- go (unembed t)
     u' <- go u
-    return $ Let (bind p u') t'
+    return $ Let (bind (p, embed t') u')
   go (Cons c ts) = do
     ts' <- mapM go ts
     return $ Cons c ts'

src/Eval.hs
@@ -74,9 +74,9 @@ evalTerm (App f t) = do
       env' <- match p vt
       local (const (env' ++ env)) (evalTerm u)
     _ -> throwError ("cannot apply " ++ show vf ++ " to " ++ show vt)
-evalTerm (Let b t) = do
-  (p, u) <- unbind b
-  vt <- evalTerm t
+evalTerm (Let b) = do
+  ((p, t), u) <- unbind b
+  vt <- evalTerm (unembed t)
   env <- match p vt
   local (env++) (evalTerm u)
 evalTerm (Match t rs) = do

src/Parser.hs
@@ -19,7 +19,7 @@ import Data.Void
 import Text.Megaparsec hiding (match)
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
-import Unbound.Generics.LocallyNameless (bind, string2Name)
+import Unbound.Generics.LocallyNameless (bind, embed, string2Name)
 
 sc = L.space (skipSome spaceChar *> pure ()) lineCmnt blockCmnt
  where
@@ -93,7 +93,7 @@ lambda = mkLam <$> s_lambda <*> some pattern <*> s_dot <*> lterm
 
 letIn = mkLets <$> s_let <*> sepBy binding s_semi <*> s_in <*> lterm
   where mkLets _ bindings _ t = foldr mkLet t bindings
-        mkLet (x, u) v = Let (bind x v) u
+        mkLet (x, u) v = Let (bind (x, embed u) v)
 
 binding = mkBinding <$> pattern <*> s_equals <*> lterm
   where mkBinding p _ t = (p, t)

src/Pretty.hs
@@ -79,8 +79,8 @@ prettyTerm par t = prettyApps par (apps t)
           $ D.group
           $ D.nest 2
           $ D.vsep ["\\" <> p' D.<+> "->", td]
-    prettyAtom par (Let b t) = lunbind b $ \(p, u) -> do
-      t' <- prettyTerm False t
+    prettyAtom par (Let b) = lunbind b $ \((p, t), u) -> do
+      t' <- prettyTerm False (unembed t)
       u' <- prettyTerm False u
       p' <- prettyPattern p
       return

commit 10a2fd301a2b3ab5f4ac6f37fdf54f3381096283

fix typo in CPS introduced by e054fb335c22e7aeba95ecf25da5ff1f8756f345

src/CPS.hs
@@ -24,7 +24,7 @@ cps defs = runFreshM $ do
   ts' <- mapM rhsCps ts
   idFun <- mkId
   t' <- eCps t idFun
-  return $ makeFunDefs (zip fs ts') t
+  return $ makeFunDefs (zip fs ts') t'
  where
   mkId = do
     x <- fresh (string2Name "x")