bc208eb246b901ae0dbb53568e833002ad2d678b
Paul Brauner <polux2001@gmail.com>
2018-10-26 15:32:31 +0200

    get typed_expr.flam to typecheck!

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 -- Copyright 2018 Google LLC
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,6 +25,7 @@ import Unbound.Generics.LocallyNameless
 import qualified Unbound.Generics.LocallyNameless.Internal.Fold as U
 import Control.Exception.Base (assert)
 import Control.Monad.Loops (allM)
+import Debug.Trace
 
 type DataConSignature = Bind Alphas (Delta, [Type], Type)
 type Sigma = [(Name DataCon, DataConSignature)]
@@ -49,8 +51,147 @@ typeCheck (FProgram b) = runFreshMT $ do
     break (DataConDecl conName sig) = (conName, unembed sig)
 
 typeCheckFTerm
-  :: MonadError String m => Sigma -> Alphas -> Delta -> Gamma -> FTerm -> m Type
-typeCheckFTerm sigma alphas delta gamma _ = undefined
+  :: (Fresh m, MonadError String m)
+  => Sigma
+  -> Alphas
+  -> Delta
+  -> Gamma
+  -> FTerm
+  -> m Type
+typeCheckFTerm sigma alphas delta gamma t = check t
+ where
+  check (FVar x) = lookupType gamma x
+  check (FLam b) = do
+    ((p, unembed->ty), t) <- unbind b
+    (alphas', delta', gamma') <- typeCheckPattern sigma alphas p ty
+    retType <- typeCheckFTerm sigma
+                              (alphas ++ alphas')
+                              (delta ++ delta')
+                              (gamma ++ gamma')
+                              t
+    return (TCons (string2Name "->") [ty, retType])
+  check (FTlam b) = do
+    (tyvar, t) <- unbind b
+    ty <- typeCheckFTerm sigma (tyvar : alphas) delta gamma t
+    return $ TForall (bind tyvar ty)
+  check (FCons con tys ts) = do
+    bsig <- lookupDataCon sigma con
+    (tvars, (constraints, argTypes, returnType)) <- unbind bsig
+    when (length tvars < length tys)
+         (throwError (show con ++ " is applied to too many type arguments"))
+    when (length tvars > length tys)
+         (throwError (show con ++ " is not applied to enough type arguments"))
+    when (length argTypes < length ts)
+         (throwError (show con ++ " is applied to too many arguments"))
+    when (length argTypes > length ts)
+         (throwError (show con ++ " is not applied to enough arguments"))
+    let sub = zip tvars tys
+        constraints' = substs sub constraints
+        argTypes' = substs sub argTypes
+        returnType' = substs sub returnType
+    tsTypes <- mapM check ts
+    checkEntailsAll alphas delta constraints'
+    zipWithM_ (checkEqTypes alphas delta) tsTypes argTypes'
+    return returnType'
+  check (FApp t1 t2) = do
+    ty1 <- check t1
+    ty2 <- check t2
+    case ty1 of
+      TCons con [u1, u2] | con == string2Name "->" -> do
+        checkEqTypes alphas delta u1 ty2
+        return u2
+      _ -> throwError
+        (toString t1 ++ " cannot be applied: it has type " ++ toString ty1)
+  check (FTApp t ty) = do
+    checkKind alphas ty KType
+    tty <- check t
+    case tty of
+      TForall b -> do
+        (tvar, retType) <- unbind b
+        return (subst tvar ty retType)
+      _ -> throwError
+        ("cannot apply "
+        ++ toString t
+        ++ " to type "
+        ++ toString ty
+        ++ ": it has type "
+        ++ toString tty
+        )
+  check (FMatch returnType t rules) = do
+    tty <- check t
+    tys <- checkRules sigma alphas delta gamma tty returnType rules
+    return returnType
+
+checkRules
+  :: (Fresh m, MonadError String m)
+  => Sigma
+  -> Alphas
+  -> Delta
+  -> Gamma
+  -> Type
+  -> Type
+  -> FRules
+  -> m ()
+checkRules sigma alphas delta gamma patternType expectedType rules =
+  mapM_ (checkRule sigma alphas delta gamma patternType expectedType) rules
+
+checkRule
+  :: (Fresh m, MonadError String m)
+  => Sigma
+  -> Alphas
+  -> Delta
+  -> Gamma
+  -> Type
+  -> Type
+  -> FRule
+  -> m ()
+checkRule sigma alphas delta gamma patternType expectedType (FRule b) = do
+  (p, t) <- unbind b
+  (alphas', delta', gamma') <- typeCheckPattern sigma alphas p patternType
+  inferredType <- typeCheckFTerm sigma (alphas ++ alphas') (delta ++ delta') (gamma ++ gamma') t
+  checkEqTypes (alphas'++alphas) (delta'++delta) inferredType expectedType
+
+typeCheckPattern
+  :: (Fresh m, MonadError String m)
+  => Sigma
+  -> Alphas
+  -> FPattern
+  -> Type
+  -> m (Alphas, Delta, Gamma)
+typeCheckPattern sigma alphas = check
+ where
+  check (FPVar x) ty = do
+    checkKind alphas ty KType
+    return ([], [], [(x, ty)])
+  check (FPCons (unembed->con) tvars ps) ty = do
+    bsig <- lookupDataCon sigma con
+    (sigTvars, (sigDelta, sigTys, sigReturnType)) <- unbind bsig
+    case (ty, sigReturnType) of
+      (TCons tyCon tyArgs, TCons sigCon sigTyArgs) | aeq tyCon sigCon -> do
+        when
+          (length sigTvars < length tvars)
+          (throwError (show con ++ " is applied to too many type arguments"))
+        when
+          (length sigTvars > length tvars)
+          (throwError (show con ++ " is not applied to enough type arguments"))
+        when (length sigTys < length ps)
+             (throwError (show con ++ " is applied to too many arguments"))
+        when (length sigTys > length ps)
+             (throwError (show con ++ " is not applied to enough arguments"))
+        (concat->alphas', concat->deltas', concat->gammas') <-
+          unzip3
+            <$> zipWithM (typeCheckPattern sigma (tvars ++ alphas)) ps sigTys
+        return
+          ( tvars ++ alphas' ++ alphas
+          , (zipWith (:~:) sigTyArgs tyArgs) ++ deltas'
+          , gammas'
+          )
+      _ -> throwError
+        ("pattern return type "
+        ++ toString sigReturnType
+        ++ " does not match expected "
+        ++ toString ty
+        )
 
 checkDef
   :: (MonadError String m, Fresh m)
@@ -65,7 +206,36 @@ checkDef sigma alphas delta gamma (x, ty, t) = do
   eq <- entails alphas delta ty ty'
   if eq
     then return ()
-    else throwError (show x ++ " does not have type " ++ toString ty)
+    else throwError
+      (show x
+      ++ " does not have type "
+      ++ toString ty
+      ++ ", it has type "
+      ++ toString ty'
+      )
+
+checkEqTypes
+  :: (MonadError String m, Fresh m) => Alphas -> Delta -> Type -> Type -> m ()
+checkEqTypes alphas delta ty1 ty2 = do
+  eq <- entails alphas delta ty1 ty2
+  if eq
+    then return ()
+    else throwError
+      ("type "
+      ++ toString ty1
+      ++ " is not equal to "
+      ++ toString ty2
+      ++ " under the assumptions "
+      ++ show delta
+      )
+
+checkEntailsAll
+  :: (MonadError String m, Fresh m) => Alphas -> Delta -> Delta -> m ()
+checkEntailsAll alphas delta1 delta2 = do
+  ok <- entailsAll alphas delta1 delta2
+  if ok
+    then return ()
+    else throwError (show delta1 ++ " does not entail " ++ show delta2)
 
 lookupDataCon
   :: MonadError String m => Sigma -> Name DataCon -> m DataConSignature
@@ -82,7 +252,7 @@ lookupType gamma x = maybe
   (lookup x gamma)
 
 
-entailsAll :: Fresh m => Alphas -> Delta -> [Constraint] -> m Bool
+entailsAll :: Fresh m => Alphas -> Delta -> Delta -> m Bool
 entailsAll alphas delta delta' = allM entailsConstraint delta'
  where
   entailsConstraint (ty1 :~: ty2) = entails alphas delta ty1 ty2
@@ -128,3 +298,9 @@ entails alphas delta ty1 ty2 | any conflict delta = return True
 
 kindCheck :: Alphas -> Type -> Kind -> Bool
 kindCheck _ _ _ = True
+
+checkKind :: MonadError String m => Alphas -> Type -> Kind -> m ()
+checkKind alphas ty k =
+  if kindCheck alphas ty k
+    then return ()
+    else throwError ("type " ++ toString ty ++ " does not have kind " ++ show k)
