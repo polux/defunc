d28ff61e4ca5067ba1ff22236e143d4bf8529e4f
Paul Brauner <polux2001@gmail.com>
2018-10-26 10:43:23 +0200

    add more scaffolding for the type checker

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -21,44 +21,84 @@ import Control.Monad.Except
 import Unbound.Generics.LocallyNameless
 import qualified Unbound.Generics.LocallyNameless.Internal.Fold as U
 import Control.Exception.Base (assert)
+import Control.Monad.Loops (allM)
+
+type DataConSignature = Bind Alphas (Delta, [Type], Type)
+type Sigma = [(Name DataCon, DataConSignature)]
+type Delta = [Constraint]
+type Alphas = [Name Type]
+type Gamma = [(Name FTerm, Type)]
 
 typeCheck :: MonadError String m => FProgram -> m Type
 typeCheck prog = throwError "type checking not yet implemented"
 
-entails :: Fresh m => [Constraint] -> Constraint -> m Bool
-entails delta eq
-  | any conflict delta = return True
-  | any occurCheck delta = return True
-  | otherwise = entails' delta eq
+typeCheckFTerm
+  :: MonadError String m => Sigma -> Alphas -> Delta -> Gamma -> FTerm -> m Type
+typeCheckFTerm sigma alphas delta gamma _ = undefined
 
+lookupDataCon
+  :: MonadError String m => Sigma -> Name DataCon -> m DataConSignature
+lookupDataCon sigma con = maybe
+  (throwError $ "data constructor " ++ show con ++ " not found")
+  return
+  (lookup con sigma)
+
+lookupType
+  :: MonadError String m => Gamma -> Name FTerm -> m Type
+lookupType gamma x = maybe
+  (throwError $ show x ++ " not found")
+  return
+  (lookup x gamma)
+
+extractSigma :: [TypeDecl] -> Sigma
+extractSigma decls = concatMap extract decls
+  where
+    extract (TypeDecl _ _ decls) = map break decls
+    break (DataConDecl conName sig) = (conName, unembed sig)
+
+entailsAll :: Fresh m => Alphas -> Delta -> [Constraint] -> m Bool
+entailsAll alphas delta delta' = allM entailsConstraint delta'
  where
-   conflict (TCons tyc1 _ :~: TCons tyc2 _) = not (tyc1 `aeq` tyc2)
-   conflict _ = False
+  entailsConstraint (ty1 :~: ty2) = entails alphas delta ty1 ty2
 
-   occurCheck (TVar a :~: ty) | not (isTVar ty) && a `elem` U.toListOf fv ty = True
-   occurCheck (ty :~: TVar a) | not (isTVar ty) && a `elem` U.toListOf fv ty = True
-   occurCheck _ = False
+entails :: Fresh m => Alphas -> Delta -> Type -> Type -> m Bool
+entails alphas delta ty1 ty2 | any conflict delta = return True
+                             | any occurCheck delta = return True
+                             | otherwise = entails' alphas delta ty1 ty2
+ where
+  conflict (TCons tyc1 _ :~: TCons tyc2 _) = not (tyc1 `aeq` tyc2)
+  conflict _ = False
 
-   isTVar (TVar _) = True
-   isTVar _ = False
+  occurCheck (TVar a :~: ty) | not (isTVar ty) && a `elem` U.toListOf fv ty =
+    True
+  occurCheck (ty :~: TVar a) = occurCheck (TVar a :~: ty)
+  occurCheck _ = False
 
-entails' [] (a :~: b) | aeq a b = return True
-entails' (TVar a :~: TVar b : delta) eq | a == b = entails' delta eq
-entails' (TVar a :~: ty : delta) eq =
+  isTVar (TVar _) = True
+  isTVar _ = False
+
+  entails' :: Fresh m => Alphas -> Delta -> Type -> Type -> m Bool
+  entails' alphas [] ty1 ty2 | aeq ty1 ty2 = return (kindCheck alphas ty1 KType)
+  entails' alphas (TVar a :~: TVar b : delta) ty1 ty2 | a == b =
+    entails' alphas delta ty1 ty2
   -- we know that ty does not contain a by IH
-  entails (subst a ty delta) (subst a ty eq)
-entails' (ty :~: TVar a : delta) eq =
+  entails' alphas (TVar a :~: ty : delta) ty1 ty2 =
+    entails alphas (subst a ty delta) (subst a ty ty1) (subst a ty ty2)
   -- we know that ty does not contain a by IH
-  entails (subst a ty delta) (subst a ty eq)
-entails' (TCons tyc1 ts1 :~: TCons tyc2 ts2 : delta) eq =
+  entails' alphas (ty :~: TVar a : delta) ty1 ty2 =
+    entails alphas (subst a ty delta) (subst a ty ty1) (subst a ty ty2)
   -- we know that tyc1 == tyc2 by IH
-  assert (tyc1 == tyc2) (entails delta' eq)
- where delta' = (zipWith (:~:) ts1 ts2) ++ delta
-entails' (TForall b1 :~: TForall b2 : delta) eq = do
-  tyc <- fresh (string2Name "C")
-  let as = U.toListOf fv delta
-  let ty = TCons tyc (map TVar as)
-  (a1, ty1) <- unbind b1
-  (a2, ty2) <- unbind b2
-  entails (subst a1 ty ty1 :~: subst a2 ty ty2 : delta) eq
-entails' _ _ = return False
+  entails' alphas (TCons tyc1 ts1 :~: TCons tyc2 ts2 : delta) ty1 ty2 = assert
+    (tyc1 == tyc2)
+    (entails alphas delta' ty1 ty2)
+    where delta' = (zipWith (:~:) ts1 ts2) ++ delta
+  entails' alphas (TForall b1 :~: TForall b2 : delta) ty1 ty2 = do
+    tyc <- fresh (string2Name "C")
+    let ty = TCons tyc (map TVar alphas)
+    (a1, u1) <- unbind b1
+    (a2, u2) <- unbind b2
+    entails alphas (subst a1 ty u1 :~: subst a2 ty u2 : delta) ty1 ty2
+  entails' _ _ _ _ = return False
+
+kindCheck :: Alphas -> Type -> Kind -> Bool
+kindCheck _ _ _ = True
